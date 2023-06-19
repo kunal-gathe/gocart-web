df52e49b85296c246734b406071f6b903032a9c9
Kunal Gathe <kunalgathe121@example.com>
2023-06-19 14:22:34 +0530

    responsive design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 
 function App() {
   return (
 	<div>
-
+	  <Grid templateAreas={{
+		base:` "nav" "main"`,
+		lg: `"nav nav" "aside main" `
+		
+	  }}>
+		<GridItem bg='red' area='nav'>Nav</GridItem>
+		<Show above='lg'>  
+		<GridItem bg='blue' area='aside'>aside</GridItem>
+		</Show>
+		<GridItem bg='purple' area='main'>main</GridItem>
+	  </Grid>
 	</div>
   )
 }
 
 export default App
+
